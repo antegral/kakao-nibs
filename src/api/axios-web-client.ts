ee6a250f5725795a8a4aaf985f3248159f72e4c0
antegral <antegral@antegral.net>
2023-01-07 18:53:50 +0900

    Add: type guard for reqData.headers

--- src/api/axios-web-client.ts
+++ src/api/axios-web-client.ts
@@ -4,8 +4,8 @@
  * Copyright (c) storycraft. Licensed under the MIT Licence.
  */
 
-import { AxiosRequestConfig } from 'axios';
-import Axios from 'axios';
+import { AxiosRequestConfig } from "axios";
+import Axios from "axios";
 import {
   HeaderDecorator,
   RequestHeader,
@@ -13,17 +13,19 @@ import {
   RequestForm,
   WebClient,
   FileRequestData,
-} from './web-client';
-import { convertToFormData } from './web-api-util';
-import FormData from 'form-data';
+} from "./web-client";
+import { convertToFormData } from "./web-api-util";
+import FormData from "form-data";
 
 /**
  * WebClient implementation wrapped with axios
  */
 export class AxiosWebClient implements WebClient, HeaderDecorator {
-  constructor(public scheme: string, public host: string, private _decorator?: HeaderDecorator) {
-
-  }
+  constructor(
+    public scheme: string,
+    public host: string,
+    private _decorator?: HeaderDecorator
+  ) {}
 
   get url(): string {
     return `${this.scheme}://${this.host}`;
@@ -34,11 +36,14 @@ export class AxiosWebClient implements WebClient, HeaderDecorator {
   }
 
   fillHeader(header: RequestHeader): void {
-    header['Host'] = this.host;
+    header["Host"] = this.host;
     this._decorator?.fillHeader(header);
   }
 
-  private buildAxiosReqData(method: RequestMethod, header?: RequestHeader): AxiosRequestConfig {
+  private buildAxiosReqData(
+    method: RequestMethod,
+    header?: RequestHeader
+  ): AxiosRequestConfig {
     const headers: RequestHeader = {};
 
     this.fillHeader(headers);
@@ -52,10 +57,10 @@ export class AxiosWebClient implements WebClient, HeaderDecorator {
       // https://github.com/axios/axios/issues/907
       transformResponse: (data) => data,
 
-      responseType: 'arraybuffer',
+      responseType: "arraybuffer",
 
       maxContentLength: 100000000,
-      maxBodyLength: 100000000
+      maxBodyLength: 100000000,
     };
 
     if (header) Object.assign(headers, header);
@@ -67,7 +72,7 @@ export class AxiosWebClient implements WebClient, HeaderDecorator {
     method: RequestMethod,
     path: string,
     form?: RequestForm,
-    headers?: RequestHeader,
+    headers?: RequestHeader
   ): Promise<ArrayBuffer> {
     const reqData = this.buildAxiosReqData(method, headers);
     reqData.url = this.toApiURL(path);
@@ -81,31 +86,38 @@ export class AxiosWebClient implements WebClient, HeaderDecorator {
     const res = await Axios.request(reqData);
 
     if (res.status !== 200) {
-      throw new Error(`Web request failed with status ${res.status} ${res.statusText}`);
+      throw new Error(
+        `Web request failed with status ${res.status} ${res.statusText}`
+      );
     }
 
     return res.data;
   }
 
   async requestMultipart(
-      method: RequestMethod,
-      path: string,
-      form?: RequestForm,
-      headers?: RequestHeader,
+    method: RequestMethod,
+    path: string,
+    form?: RequestForm,
+    headers?: RequestHeader
   ): Promise<ArrayBuffer> {
     const reqData = this.buildAxiosReqData(method, headers);
     reqData.url = this.toApiURL(path);
 
-    if (form) {
+    if (form && reqData?.headers) {
       const formData = this.convertToMultipart(form);
+
       Object.assign(reqData.headers, formData.getHeaders());
       reqData.data = formData.getBuffer();
+    } else {
+      throw new Error(`Web request failed with undefined form or reqData`);
     }
 
     const res = await Axios.request(reqData);
 
     if (res.status !== 200) {
-      throw new Error(`Web request failed with status ${res.status} ${res.statusText}`);
+      throw new Error(
+        `Web request failed with status ${res.status} ${res.statusText}`
+      );
     }
 
     return res.data;
@@ -115,12 +127,16 @@ export class AxiosWebClient implements WebClient, HeaderDecorator {
     const formData = new FormData();
 
     for (const [key, value] of Object.entries(form)) {
-      if (value && (value as FileRequestData).value && (value as FileRequestData).options) {
+      if (
+        value &&
+        (value as FileRequestData).value &&
+        (value as FileRequestData).options
+      ) {
         const file = value as FileRequestData;
 
         formData.append(key, Buffer.from(file.value), file.options);
       } else {
-        formData.append(key, value + '');
+        formData.append(key, value + "");
       }
     }
 
